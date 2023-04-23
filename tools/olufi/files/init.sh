10f6dfbf7261f69f67026daf1209adb076522667
BrentGruber <brent.gruber77@gmail.com>
2023-04-23 00:12:34 -0400

    Olufi working, using doppler for github pat

---
+++ tools/olufi/files/init.sh
@@ -0,0 +1,44 @@
+#cloud-boothook
+#!/bin/bash
+
+sudo apt update
+
+
+# Install ssm agent
+sudo snap install amazon-ssm-agent --classic
+
+# Install pulumi
+curl -fsSL https://get.pulumi.com | sh
+echo 'export PATH=/.pulumi/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=/.pulumi/bin:$PATH' >> /home/ubuntu/.bashrc
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install npm
+sudo apt install npm
+
+# Install docker
+sudo apt install docker.io
+sudo usermod -aG docker ubuntu
+
+# Install actions-runner
+sudo -u ubuntu mkdir /home/ubuntu/actions-runner
+sudo -u ubuntu cd /home/ubuntu/actions-runner
+
+sudo apt install jq
+GITHUB_RUNNER_VERSION=$(curl --silent "https://api.github.com/repos/actions/runner/releases/latest" | jq -r '.tag_name[1:]') && \
+    sudo -u ubuntu curl -o /home/ubuntu/actions-runner/actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${GITHUB_RUNNER_VERSION}/actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz && \
+    sudo -u ubuntu tar xzf /home/ubuntu/actions-runner/actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz -C /home/ubuntu/actions-runner
+
+registration_url="https://github.com/${GITHUB_PROJECT}"
+token_url="https://api.github.com/orgs/${GITHUB_PROJECT}/actions/runners/registration-token"
+
+payload=$(curl -sX POST -H "Authorization: Bearer ${GITHUB_PAT}" ${token_url})
+runner_token=$(echo $payload | jq .token --raw-output)
+
+sudo -u ubuntu EC2_INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id` bash -c 'cd /home/ubuntu/actions-runner/;./config.sh --url https://github.com/BOM-DEMO --token ${runner_token} --name "${runner_name}-$${EC2_INSTANCE_ID}" --work _work --labels ${labels} --runasservice'
+
+
+source ~/.bashrc
--- tools/olufi/index.ts
+++ tools/olufi/index.ts
@@ -1,6 +1,5 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as fs from 'fs';
 
 
 // Get the id for the latest Ubuntu AMI
@@ -8,26 +7,65 @@ import * as awsx from "@pulumi/awsx";
 // and actions steps can run on the same os
 const ami = aws.ec2.getAmi({
     filters: [
-        { name: "name", values: ["ubuntu/images/ubuntu-*-*-amd64-server-*"]},
+        { name: "name", values: ["ubuntu/images/*jammy*amd64-server*"]},
     ],
-    owners: ["099720109477"], //Amazon
+    owners: ["099720109477"], //Canonical
     mostRecent: true,
 }).then(result => result.id);
 
 // create a new security group for port 80
 // may not be needed for actions runner, but will keep it here for now
-const group = new aws.ec2.securityGroup("olufi-secgrp", {
+const group = new aws.ec2.SecurityGroup("olufi-secgrp", {
     ingress: [
         { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] }
     ],
+    egress: [{ protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["0.0.0.0/0"] }],
 });
+
+// create an iam role assumable by ec2
+// Todo get the managedPolicyArns from the existing role instead of hardcoding the policies
+const olufiRole = new aws.iam.Role("olufi-role", {
+    assumeRolePolicy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [{
+            Action: "sts:AssumeRole",
+            Effect: "Allow",
+            Sid: "",
+            Principal: {
+                Service: "ec2.amazonaws.com",
+            },
+        }],
+    }),
+    managedPolicyArns: [
+        "arn:aws:iam::593393184947:policy/L1DevelopersDenyAccess",
+        "arn:aws:iam::593393184947:policy/L1GlobalDenyAccess",
+        "arn:aws:iam::593393184947:policy/L1Regions",
+        "arn:aws:iam::593393184947:policy/L1UsersDenyAccess1of2",
+        "arn:aws:iam::593393184947:policy/L1UsersDenyAccess2of2",
+        "arn:aws:iam::aws:policy/AdministratorAccess",
+        "arn:aws:iam::aws:policy/AWSSupportAccess",
+        "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    ]
+});
+
+// Create the machine profile so the ec2 can assume the iam role
+const machineProfile = new aws.iam.InstanceProfile("olufi-profile", {role: olufiRole.name});
+
+// replace environment variables in script
+var startupScript = fs.readFileSync('files/init.sh', 'utf-8');
+startupScript = startupScript.replace("${GITHUB_PAT}", process.env.GITHUB_PAT!)
+
+
 
 // Create the ec2 instance
 const server = new aws.ec2.Instance("olufi-server", {
     tags: { "Name": "olufi-server", "Owner": "Brent", "ManagedBy": "Pulumi"},
-    instanceType: aws.ec2.InstanceType.T2_Micro, // may need to beef this up
+    instanceType: aws.ec2.InstanceType.T2_Large, // may need to beef this up
     vpcSecurityGroupIds: [ group.id ], // reference the group object above
-    ami: ami
+    ami: ami,
+    iamInstanceProfile: machineProfile.name,
+    userData: startupScript
 });
 
 export const publicIp = server.publicIp;
